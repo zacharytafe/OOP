f7fb125d5b6907665316bbc0c82490bfc71dfb36
zachary.dumlao1 <zachary.dumlao1@600900B211DE406>
2018-02-28 11:57:55 +1100

    Started ~Asteroids project

---
+++ Assets/~Asteroids/Scripts/Moving.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids
+{
+    public class Moving : MonoBehaviour
+    {
+        public float acceleration = 5f;
+        public float rotationSpeed = 5f;
+        public float maxVelocity = 3f;
+
+        private Rigidbody2D rigid;
+
+        // Use this for initialization
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            LimitVelocity();
+        }
+
+        //Capping the velocity when it goes too high
+        void LimitVelocity()
+        {
+            Vector3 vel = rigid.velocity;
+            if(vel.magnitude > maxVelocity)
+            {
+                vel = vel.normalized * maxVelocity;
+            }
+            // Applies the copied varible to velocity
+            rigid.velocity = vel;
+        }
+
+        public void Accelerate(Vector3 direction)
+        {
+            rigid.AddForce(direction * acceleration);
+        }
+
+        public void Rotate(float angleOffset)
+        {
+            rigid.rotation -= angleOffset * rotationSpeed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/~Asteroids/Scripts/Shooting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteriods
+{ 
+public class Shooting : MonoBehaviour 
+{ 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+}
