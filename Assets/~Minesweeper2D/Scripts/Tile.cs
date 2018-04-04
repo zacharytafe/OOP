ee5a7ea3100152206c40ad386ed3028776366bc8
zachary.dumlao1 <zachary.dumlao1@600900B211DE406>
2018-04-04 12:00:44 +1000

    Added ~Minesweeper game, scripts, prefabs, sprites and other stuff!

---
+++ Assets/~Minesweeper2D/Scripts/Tile.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minesweeper2D
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Tile : MonoBehaviour
+    {
+        // Store x and y coordinate in array for later use
+        public int x = 0;
+        public int y = 0;
+        public bool isMine = false;
+        public bool isRevealed = false;
+        [Header("References")]
+        public Sprite[] emptySprites;
+        public Sprite[] mineSprites;
+
+        private SpriteRenderer rend;
+
+       
+        void Awake()
+        {
+            // Grab reference to sprite renderer
+            rend = GetComponent<SpriteRenderer>();
+        }
+
+        void Start()
+        {
+            isMine = Random.value < .05f;
+        }
+
+        public void Reveal(int adjacentMines, int mineState = 0)
+        {
+            // Flags the tile as being revealed
+            isRevealed = true;
+            // Check if the tile is a mine
+            if (isMine)
+            {
+                // Sets sprite to mine sprite
+                rend.sprite = mineSprites[mineState];
+            }
+            else
+            {
+                // Sets sprites to appropriate texture based on adjacent mines
+                rend.sprite = emptySprites[adjacentMines];
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
